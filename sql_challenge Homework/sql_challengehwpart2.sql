0088839951f5a3cb64c0366fb79abf4a086ee12a
vbmoreno <61644571+vbmoreno@users.noreply.github.com>
2020-05-14 01:09:47 -0400

    Add files via upload

---
+++ sql_challenge Homework/sql_challengehwpart2.sql
@@ -0,0 +1,61 @@
+CREATE TABLE departments (
+    dept_no VARCHAR(255)   NOT NULL,
+    dept_name VARCHAR(255)   NOT NULL,
+    CONSTRAINT pk_departments PRIMARY KEY (
+        dept_no
+     )
+);
+
+CREATE TABLE titles (
+    title_id VARCHAR(255)   NOT NULL,
+    title VARCHAR(255)   NOT NULL,
+    CONSTRAINT pk_titles PRIMARY KEY (
+        title_id
+     )
+);
+	
+CREATE TABLE employees (
+    emp_no VARCHAR(255)   NOT NULL,
+    emp_title_id VARCHAR(255)   NOT NULL,
+    birth_date DATE   NOT NULL,
+    first_name VARCHAR(255)   NOT NULL,
+    last_name VARCHAR(255)   NOT NULL,
+    sex VARCHAR(255)   NOT NULL,
+    hire_date DATE   NOT NULL,
+    CONSTRAINT pk_employees PRIMARY KEY (
+        emp_no
+     )
+);
+
+CREATE TABLE dept_manager (
+    dept_no VARCHAR(255)   NOT NULL,
+    emp_no VARCHAR(255)   NOT NULL
+);
+
+CREATE TABLE dept_employees (
+    emp_no VARCHAR(255)   NOT NULL,
+    dept_no VARCHAR(255)   NOT NULL
+);
+
+CREATE TABLE salaries (
+    emp_no VARCHAR(255)   NOT NULL,
+    salary INT   NOT NULL
+);
+
+ALTER TABLE dept_employees ADD CONSTRAINT fk_dept_employees_dept_no FOREIGN KEY (dept_no)
+REFERENCES public.departments (dept_no);
+
+ALTER TABLE employees ADD CONSTRAINT fk_employees_title_id FOREIGN KEY(emp_title_id)
+REFERENCES public.titles (title_id);
+
+ALTER TABLE dept_employees ADD CONSTRAINT fk_dept_employees_emp_no FOREIGN KEY (emp_no)
+REFERENCES public.employees (emp_no);
+
+ALTER TABLE dept_manager ADD CONSTRAINT fk_dept_manager_emp_no FOREIGN KEY(emp_no)
+REFERENCES public.employees (emp_no);
+
+ALTER TABLE dept_manager ADD CONSTRAINT fk_dept_manager_dept_no FOREIGN KEY(dept_no)
+REFERENCES public.departments (dept_no);
+
+ALTER TABLE salaries ADD CONSTRAINT fk_salaries_emp_no FOREIGN KEY(emp_no)
+REFERENCES public.employees (emp_no);
